6257e771752e2409615f0657a250bf57da95dd43
Evgeniy Burlachenko <revan730@gmail.com>
2016-05-05 16:03:49 +0300

    Added structures header file

---
+++ structures.h
@@ -0,0 +1,33 @@
+
+enum Month {JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,NOV,OCT,DEC};
+enum Form {BUDGET,CONTRACT};
+
+struct FullName
+{
+	char[20] Surname;
+	char[20] Name;
+	char[20] Middlename;
+}
+
+struct Teacher
+{
+	struct FullName Fio;
+	Month Mon;
+	unsigned short Day;
+	unsigned short Year;
+	char[20] Position;
+}
+
+struct Student
+{
+	struct FullName	Fio;
+	unsigned short Average;
+	Form F;
+}
+
+union Person
+{
+	struct Student;
+	struct Teacher;
+}
+
