91ec0b4141f1263e3ad5a90b78cdd66a24e3c408
Evgeniy Burlachenko <revan730@gmail.com>
2016-05-10 23:31:15 +0300

    Added and moved some functions,input is bugged

--- main.cpp
+++ main.cpp
@@ -1,19 +1,54 @@
 #include <stdio.h>
+#include <iostream>
 #include "structures.h"
 #include <string.h>
 #define gsize 4
 
 int writeToFile();
 int readFromFile();
+void inputGroup(int);
+void inputStudent(Student*);
 void inputStudents(Group *g);
 void inputCurator(Group *g);
+void mainMenu();
+void createFile();
+void appendGroup();
 
-
-struct Group groups[gsize];
-
+//struct Group groups[gsize];
+struct Group *groups;
 int main()
 {
+    mainMenu();
 	return 0;
+}
+
+void mainMenu()
+{
+    while (true)
+    {
+        int i;
+        printf("1.Create file\n2.Open file\n3.Close\n");
+        scanf("%d",&i);
+        switch(i)
+        {
+        case 1:
+            createFile();
+            break;
+        case 2:
+            //int n;
+            //printf("Input number of groups\n");
+            //scanf("%d",&n);
+            //groups = new Group[n];
+            readFromFile();
+            break;
+        case 3:
+            return;
+            break;
+        default:
+            printf("Input error\n");
+            break;
+        }
+    }
 }
 
 int writeToFile()
@@ -42,47 +77,72 @@ int readFromFile()
 	return 0;
 }
 
+void createFile()
+{
+    int n;
+    printf("Введите количество групп\n");
+    //scanf("%d",&n);
+    std::cin >> n;
+    groups = new Group[n];
+    for (int i = 0;i < n;i++)
+        inputGroup(n);
+    writeToFile();
+    //delete [] groups;
+}
+
 void inputGroup(int n)//Input information about n-th group
 {
+    char name[7];
+    printf("Input group name\n");
+	gets(name);
+	strcpy(groups[n].Name,name);
 	printf("Input number of students\n");
-	scanf("%d",&groups[n].size);
+	//scanf("%d",&groups[n].size);
+	std::cin >> groups[n].size;
 	inputStudents(&groups[n]);
 	inputCurator(&groups[n]);
 }
 
-void inputStudents(Group *g)
+void inputStudents(Group *g)//Input students of specific group
 {
     for (int i = 0;i < g->size;i++)
 	{
-		char name[20];
-		float m;
-		int f;
-		printf("Input %d-nt's student name\n",i + 1);
-		gets(name);
-		strcpy(g->Studs[i].S.Fio.Name,name);
-		printf("Input surname\n");
-		gets(name);
-		strcpy(g->Studs[i].S.Fio.Surname,name);
-		printf("Input middle name\n");
-		gets(name);
-		strcpy(g->Studs[i].S.Fio.Middlename,name);
-		printf("Input average mark\n");
-		scanf("%f",&m);
-		printf("0 - budget form (default),1 - contract form");
-		scanf("%d",&f);
-		switch (f)
-		{
+	    inputStudent(&g->Studs[i].S);
+
+	}
+}
+
+void inputStudent(Student *stud)//Input specific student's info
+{
+    char name[20];
+    float m;
+    int f;
+    printf("Input student's name\n");
+    gets(name);
+    strcpy(stud->Fio.Name,name);
+    printf("Input surname\n");
+    gets(name);
+    strcpy(stud->Fio.Surname,name);
+    printf("Input middle name\n");
+    gets(name);
+    strcpy(stud->Fio.Middlename,name);
+    printf("Input average mark\n");
+    scanf("%f",&m);
+    printf("0 - budget form (default),1 - contract form\n");
+    //scanf("%d",&f);
+    std::cin >> f;
+    switch (f)
+    {
         case 0:
-            g->Studs[i].S.F = BUDGET;
+            stud->F = BUDGET;
             break;
         case 1:
-            g->Studs[i].S.F = CONTRACT;
+            stud->F = CONTRACT;
             break;
         default:
-            g->Studs[i].S.F = BUDGET;
+            stud->F = BUDGET;
             break;
-		}
-	}
+    }
 }
 
 void inputCurator(Group *g)
@@ -97,8 +157,18 @@ void inputCurator(Group *g)
     printf("Input middle name\n");
     gets(name);
     strcpy(g->Curator.T.Fio.Middlename,name);
-    printf("Input curator's position");
+    printf("Input curator's position\n");
     gets(name);
     strcpy(g->Curator.T.Position,name);
     //TODO Birthday input
+}
+
+void editStudent(Group *g,int n)
+{
+    inputStudent(&g->Studs[n].S);
+}
+
+void appendGroup()
+{
+    //Placeholder
 }
